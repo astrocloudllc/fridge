cfe9b25aecab31a60f52f1f07c0a22020a045577
Aaron Ingalls <aaroningalls@hotmail.com>
2025-01-05 16:20:41 -0600

    Add jsdoc comments

--- src/index.ts
+++ src/index.ts
@@ -9,12 +9,6 @@ export const createStorage = <
   Keys extends Record<string, any>
 >(): TypedStorage<Keys> => {
   return {
-    /**
-     * Read an item from local storage
-     *
-     * @param {string} key The key where the data is stored
-     * @returns Resolves object type `T` if found, throws `null` otherwise
-     */
     getItem: async <T extends Key<Keys>>(key: T) => {
       const data = await AsyncStorage.getItem(key);
       if (data === null) {
@@ -23,12 +17,6 @@ export const createStorage = <
         return JSON.parse(data) as Keys[T];
       }
     },
-    /**
-     * Read an item, but returns `null` instead of throwing an error
-     *
-     * @param key The key where the data is stored
-     * @returns Object type `T` if found, `null` otherwise
-     */
     getNullableItem: async <T extends Key<Keys>>(key: T) => {
       const data = await AsyncStorage.getItem(key);
       if (data === null) {
@@ -37,63 +25,13 @@ export const createStorage = <
         return JSON.parse(data) as Keys[T];
       }
     },
-    /**
-     * Read an arbitray key for one-offs that aren't in the `StorageKeys` type. Behaves identically to `AsyncStorage.getItem()`
-     *
-     * @param key The key where the data is stored
-     * @returns String if found, `null` otherwise
-     */
-
     getKey: async (key: string) => AsyncStorage.getItem(key),
-    /**
-     * Set an item to local storage
-     *
-     * @param {string} key The key of where to store the data
-     * @param {T} data The object to store in local storage
-     *
-     */
     setItem: <T extends Key<Keys>>(key: T, data: Keys[T]) =>
       AsyncStorage.setItem(key, JSON.stringify(data)),
-    /**
-     * Set an arbitrary key. Behaves identically to `AsyncStorage.setItem()`
-     *
-     * @param key The key of where to store the data
-     * @param data The string to be stored
-     *
-     */
     setKey: (key: string, data: string) => AsyncStorage.setItem(key, data),
-    /**
-     * Merges an existing `key` value with an input value, assuming both values are stringified JSON.
-     *
-     * @param {string} key The key where the data is being stored.
-     * @param {T} data Data to be merged with current data
-     */
     mergeItem: <T extends Key<Keys>>(key: T, data: DeepPartial<Keys[T]>) => {
-      // const cur = await this.getItem<T>(key);
-      // if (Array.isArray(cur) && Array.isArray(data)) {
-      //   console.log("both arrays");
-      //   const newArray = Array.from(new Set([...cur, ...data]));
-      //   console.log(newArray);
-      //   return AsyncStorage.setItem(key, JSON.stringify(newArray));
-      // } else if (
-      //   Object.entries(cur as object).length !== 0 &&
-      //   Object.entries(data as object).length !== 0
-      // ) {
-      //   return AsyncStorage.setItem(key, JSON.stringify({ ...cur, ...data }));
-      // } else {
-      //   throw new Error("incompatable type(s)");
-      // }
       return AsyncStorage.mergeItem(key, JSON.stringify(data));
     },
-    /**
-     * Set data for multiple keys
-     * @param data The data to be stored, passed in the form
-     * ```ts
-     *  {
-     *    [key: Key]: data
-     *  }
-     * ```
-     */
     multiSet: async (data: Partial<Keys>) => {
       const keyValues: [string, string][] = [];
       Object.keys(data).forEach((e) => {
@@ -101,17 +39,6 @@ export const createStorage = <
       });
       return AsyncStorage.multiSet(keyValues);
     },
-    /**
-     * Read multiple keys and return them as an object
-     *
-     * @param {string[]} keys The keys to read
-     * @returns An object of the form
-     * ```ts
-     *  {
-     *    [key: Key]: data
-     *  }
-     * ```
-     */
     multiGet: async <T extends keyof Keys>(keys: T[]) => {
       const data = await AsyncStorage.multiGet(keys as string[]);
       let values = {} as MultiGetObject<Keys, T>;
@@ -122,27 +49,13 @@ export const createStorage = <
       });
       return values;
     },
-    /**
-     * Removes an item from local storage
-     *
-     * @param {string} key The key to remove
-     */
     removeItem: (key: Key<Keys>) => AsyncStorage.removeItem(key),
-
-    /**
-     * Removes an arbitrary key from storage
-     * @param key The key to remove
-     *
-     */
     removeKey: (key: string) => AsyncStorage.removeItem(key),
-    /** Get all keys known to local storage returned as an array of strings */
     getAllKeys: async () => {
       const keys = await AsyncStorage.getAllKeys();
       return keys as Key<Keys>[];
     },
-    /** Clear local storage */
     clear: () => AsyncStorage.clear(),
-    /** Remove multiple keys */
     multiRemove: (keys: Key<Keys>[]) => AsyncStorage.multiRemove(keys),
   };
 };
--- src/types.ts
+++ src/types.ts
@@ -10,20 +10,91 @@ export type MultiGetObject<T, K extends keyof T> = {
 export type Key<T> = keyof T & string;
 
 export interface TypedStorage<T extends Record<string, any>> {
+  /**
+   * Read an item from local storage
+   *
+   * @param {string} key The key where the data is stored
+   * @returns Resolves object type `T` if found, throws `null` otherwise
+   */
   getItem: <K extends Key<T>>(key: K) => Promise<T[K]>;
+  /**
+   * Read an item, but returns `null` instead of throwing an error
+   *
+   * @param key The key where the data is stored
+   * @returns Object type `T` if found, `null` otherwise
+   */
   getNullableItem: <K extends Key<T>>(key: K) => Promise<T[K] | null>;
+  /**
+   * Read an arbitray key for one-offs that aren't in the `StorageKeys` type. Behaves identically to `AsyncStorage.getItem()`
+   *
+   * @param key The key where the data is stored
+   * @returns String if found, `null` otherwise
+   */
   getKey: (key: string) => Promise<string | null>;
+  /**
+   * Set an item to local storage
+   *
+   * @param {string} key The key of where to store the data
+   * @param {T} data The object to store in local storage
+   *
+   */
   setItem: <K extends Key<T>>(key: K, data: T[K]) => Promise<void>;
+  /**
+   * Set an arbitrary key. Behaves identically to `AsyncStorage.setItem()`
+   *
+   * @param key The key of where to store the data
+   * @param data The string to be stored
+   *
+   */
   setKey: (key: string, data: string) => Promise<void>;
+  /**
+   * Merges an existing `key` value with an input value, assuming both values are stringified JSON.
+   *
+   * @param {string} key The key where the data is being stored.
+   * @param {T} data Data to be merged with current data
+   */
   mergeItem: <K extends Key<T>>(
     key: K,
     data: DeepPartial<T[K]>
   ) => Promise<void>;
+  /**
+   * Removes an item from local storage
+   *
+   * @param {string} key The key to remove
+   */
   removeItem: <K extends Key<T>>(key: K) => Promise<void>;
+  /**
+   * Removes an arbitrary key from storage
+   * @param key The key to remove
+   *
+   */
   removeKey: (key: string) => Promise<void>;
+  /**
+   * Set data for multiple keys
+   * @param data The data to be stored, passed in the form
+   * ```ts
+   *  {
+   *    [key: Key]: data
+   *  }
+   * ```
+   */
   multiSet: (data: Partial<T>) => Promise<void>;
+  /**
+   * Read multiple keys and return them as an object
+   *
+   * @param {string[]} keys The keys to read
+   * @returns An object of the form
+   * ```ts
+   *  {
+   *    [key: Key]: data
+   *  }
+   * ```
+   */
   multiGet: <K extends Key<T>>(keys: K[]) => Promise<MultiGetObject<T, K>>;
+  /** Remove multiple keys */
   multiRemove: <K extends Key<T>>(keys: K[]) => Promise<void>;
+  /** Get all keys known to local storage returned as an array of strings */
   getAllKeys: () => Promise<Key<T>[]>;
+  /** Clear local storage */
   clear: () => Promise<void>;
 }
